c5f11319132d093e2bc1a36b2729e15366165771
Philippe Marschall <philippe.marschall@gmail.com>
2024-12-06 18:11:18 +0100

    allocation profile

--- run-with-profiler.sh
+++ run-with-profiler.sh
@@ -8,7 +8,8 @@ ${JAVA_HOME}/bin/java \
   -XX:+UnlockExperimentalVMOptions -XX:+UseEpsilonGC \
   -XX:+HeapDumpOnOutOfMemoryError \
   -XX:+AlwaysPreTouch \
+  -agentpath:${HOME}/bin/async-profiler/async-profiler-3.0-linux-x64/lib/libasyncProfiler.so=start,alloc=100k,file=async-profiler-memory.jfr \
   --enable-preview \
   --module-path target/is-latin1-1.0.0-SNAPSHOT.jar \
   --module com.github.marschall.islatinone \
-  /tmp/ISO885918727130585629604208.txt
+  /tmp/ISO885912046423853045236867.txt
